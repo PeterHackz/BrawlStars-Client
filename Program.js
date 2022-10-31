d2361352b1606b80918fd44cf41eb0058cffc67d
HaccerCat <86655602+HaccerCat@users.noreply.github.com>
2022-10-31 11:25:17 +0200

    Add files via upload

---
+++ Program.js
@@ -0,0 +1,33 @@
+var args = process.argv.slice(2);
+
+require("./utils/Debugger"); // initialize it globally
+
+if (args.length < 2) {
+    Debugger.fatal("expected 2 args: <domain> <port> <optional: dump>");
+}
+
+if (isNaN(args[1])) {
+    Debugger.fatal("port should be an integer, received", args[1]);
+}
+
+if (args[2] == "dump") {
+    global.dump = true;
+}
+
+global.String.prototype.format = function(...args) {
+    return args.reduce((p, c) => p.replace(/{}/, c), this);
+}
+
+var net = require("net"),
+Connection = require("./TcpSocket/Connection");
+
+var client = new net.Socket();
+
+client.connect(parseInt(args[1]), args[0], () => {
+    Debugger.info("succesfully connected to {}:{}".format(args[0], args[1]));
+    Connection(client);
+});
+
+client.on("error", error => {
+    Debugger.fatal(error);
+});
