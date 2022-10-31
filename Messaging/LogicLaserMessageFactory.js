423a252369c309d76375b49617a3a0aacc835efb
HaccerCat <86655602+HaccerCat@users.noreply.github.com>
2022-10-31 11:54:20 +0200

    removed fs

--- Messaging/LogicLaserMessageFactory.js
+++ Messaging/LogicLaserMessageFactory.js
@@ -1,16 +1,6 @@
-var fs = require("fs");
-
-var Messages = {}
-
-var path = process.argv[1].split("/").slice(0, -1).join("/");
-
-var files = fs.readdirSync(path+"/Messaging/Messages/Server");
-files.forEach(file => {
-    if (file.endsWith(".js")) {
-        var message = require("./Messages/Server/{}".format(file));
-        Messages[message.getMessageType()] = message;
-    }
-});
+var Messages = {
+    20100: require("./Messages/Server/ServerHelloMessage")
+}
 
 function createMessageByType(type) {
     if (Messages[type]) {
