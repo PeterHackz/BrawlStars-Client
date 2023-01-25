df6df80ea00865cde1b2d337789485c13be56696
HaccerCat <86655602+HaccerCat@users.noreply.github.com>
2023-01-25 20:35:31 +0200

    Update Program.js

--- Program.js
+++ Program.js
@@ -5,8 +5,6 @@ require("./utils/Debugger"); // initialize it globally
 if (!args[0]) {
     args[0] = "game.brawlstarsgame.com"
     Debugger.error("you forget paste ip arg! Setting an default game.brawlstarsgame.com")
-}else if(args[0] == "dump"){
-    global.dump = true
 }
 
 if (isNaN(args[1])) {
@@ -14,7 +12,7 @@ if (isNaN(args[1])) {
     Debugger.error("port should be an integer, received", args[1], ". Setting an default 9339");
 }
 
-global.dump = args[2] == "dump";
+global.dump = args[2] == "dump" || args[0] == "dump";
 
 global.String.prototype.format = function(...args) {
     return args.reduce((p, c) => p.replace(/{}/, c), this);
