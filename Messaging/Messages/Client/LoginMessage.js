65a57291591384c5e8708469e11bcc04a28244f3
kubune <132983498+kubune@users.noreply.github.com>
2024-11-15 22:47:38 +0100

    Update Supercell ID Token Field to use writeCompressedString function from Bytestream

--- Messaging/Messages/Client/LoginMessage.js
+++ Messaging/Messages/Client/LoginMessage.js
@@ -35,7 +35,7 @@ module.exports = class {
         this.ByteStream.writeString();
         this.ByteStream.writeString();
 
-        this.ByteStream.writeString();
+        this.ByteStream.writeCompressedString(); // Supercell ID Token
 
         this.ByteStream.writeBoolean(false);
         this.ByteStream.writeString();
